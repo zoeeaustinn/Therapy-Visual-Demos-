0f7d87c385afef10dcdec1a4f0121eb5262bb2c2
ZOE TALAMANTEZ <zt23f@fsu.edu>
2024-10-07 10:05:22 -0400

    updated some buttons, and edit features

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let x;
-let y; 
+let y;  
 
 let r;
 let g;
 let b;
 
 let input;
-let button;
-let button2;
+let button; let button2; let button3;
 let toggleButton;
+let gui;
+//let slider; let slider2; let slider3;
+
+//let topCanvas;
+//let showTopCanvas = true;
+
 
 
 let inputVisible = false; //hides text box, buttons, and toggles
@@ -16,9 +21,17 @@ let isMoving = true;
 
 
 function setup() {
-  createCanvas(1400, 725);
-    x = width / 2;
-    y = height / 2;
+
+// canvas underneath
+createCanvas(1400, 725);
+    
+
+//canvas on top 
+//topCanvas = createGraphics(1400, 725);
+
+
+x = width / 2;
+y = height / 2;
 
 
 r = random(255);
@@ -27,9 +40,11 @@ b = random(255);
 
 
 
-     background(207);
+background(245, 245, 220);
     
-pointSize = 20;//size of line
+pointSize = 50;//size of line
+
+
 
 
     //creates input (TEXT BOX)
@@ -71,12 +86,26 @@ button2.mousePressed(onButtonPress);
     // hides button
 button2.hide();
 
-    // creates SUBMIT toggle
+
+button3 = createButton('🖍️');
+    //where the button is positioned 
+button3.position (1125, 740);
+    //button size
+button3.size(115, 75);
+    //button text size
+button3.style('font-size', '25px');
+    //adds mousepress to button
+button3.mousePressed(onButton3Press);
+    // hides button
+button3.hide();
+
+
+    // creates DONE toggle
 toggleButton = createButton ('DONE');
     //position of toggle
-toggleButton.position (1125, 750);
+toggleButton.position (1250, 740);
     // set toggle size
-toggleButton.size(250, 55);
+toggleButton.size(115, 75);
     //set toggle font size
 toggleButton.style('font-size','25px');
     //event for toggle
@@ -84,17 +113,39 @@ toggleButton.mousePressed(onTogglePress);
     //adds mousepress to toggle 
 toggleButton.hide();
 
+
+slider = createSlider("Slider",128, 32, 0, 1);
+slider.position (1400, 0);
+slider.size (250,55);
+slider.hide();
+
+slider2 = createSlider("Slider",128, 32, 0, 1);
+slider2.position (1400, 100);
+slider2.size (250,55);
+slider2.hide();
+
+
+slider3 = createSlider("Slider",128, 32, 0, 1);
+slider3.position (1400, 200);
+slider3.size (250,55);
+slider3.hide();
+
+
+
 }
+
+
 
 
 
 function draw() {
 
+//pointSize = slider.value();
 
-    
+
     if (isMoving){
   // adjust the rate of the speed of the point - i < 10
-    for (let i = 0; i < 15; i++){
+    for (let i = 0; i < 1; i++){
         step();
     }
     } else {
@@ -103,6 +154,16 @@ function draw() {
         strokeWeight(pointSize);
         point(x, y);
     }
+    
+    //if (showTopCanvas){
+      //topCanvas.background(255, 200, 200, 150);
+     // topCanvas.fill(0);
+      //topCanvas.textSize(32);
+      //topCanvas.text('Top Canvas Working', 100, 100);
+
+     // image(topCanvas, 0, 0);
+   /// }
+
 }
 
 function onButtonPress() {
@@ -110,57 +171,59 @@ function onButtonPress() {
     console.log("button pressed");
 }
 
+function onButton3Press(){ 
+  slider.show();
+  slider2.show();
+  slider3.show();
+}
+
+
 function keyPressed(){
     if (!inputVisible) {
         input.show();
         button.show();
         button2.show();
+        button3.show();
         toggleButton.show();
+        
         inputVisible = true;
     }
 
-//when '' key is pressed, colors will change on the canvas
-if (key === 'a'){ 
-r = 255; g = 255; b = 255; // white
-}else if (key === 'e'){
-r = 255; g = 0; b = 0; // 
-}else if (key === 'i'){
-r = 255; g = 128; b = 0; // 
-}else if (key === 'o'){
-r = 255; g = 255; b = 0; //
-}else if (key === 'u'){
-r = 128; g = 255; b = 0; // 
-}else if (key === 'h'){
-r = 0; g = 255; b = 0; //  
-}else if (key === 't'){
-r = 0; g = 255; b = 128; //
-}else if (key === 'b'){
-r = 0; g = 252; b = 255; // 
-}else if (key === 'r'){
-r = 0; g = 125; b = 255; // 
-}else if (key === 's'){
-r = 3; g = 0; b = 255; // 
-}else if (key === 'm'){ 
- r = 131; g = 0; b = 255; //
-}else if (key === 'n'){
-r = 255; g = 0; b = 252; //
-}else if (key === 'l'){
-r = 255; g = 0; b = 125; // 
-}
-}
 
+//when '' key is pressed, colors will change on the canvas
+if (key === 'a'){ r = 255; g = 255; b = 255; // white
+}else if (key === 'e'){r = 255; g = 0; b = 0; // 
+}else if (key === 'i'){r = 255; g = 128; b = 0; // 
+}else if (key === 'o'){r = 255; g = 255; b = 0; //
+}else if (key === 'u'){r = 128; g = 255; b = 0; // 
+}else if (key === 'h'){r = 0; g = 255; b = 0; //  
+}else if (key === 't'){r = 0; g = 255; b = 128; //
+}else if (key === 'b'){r = 0; g = 252; b = 255; // 
+}else if (key === 'r'){r = 0; g = 125; b = 255; // 
+}else if (key === 's'){r = 3; g = 0; b = 255; // 
+}else if (key === 'm'){r = 131; g = 0; b = 255; //
+}else if (key === 'n'){r = 255; g = 0; b = 252; //
+}else if (key === 'l'){r = 255; g = 0; b = 125; // 
+} else {
+//If any other key is pressed it will change to random colors.
+r = random(255);
+g = random(255);
+b = random(255);
+
+}
+}
 // function for toggle button
 function onTogglePress(){
-    isMoving = false; //stops the point from moving 
+    isMoving = false; //stops the point from moving
+    //showTopCanvas = false; 
 }
-
 
 
 function step(){
 
     //Moves the point
-    x += random (-5, 5);
-    y += random (-5, 5);
+    x += random (-20, 20);
+    y += random (-20, 20);
 
     // Keeps line inside the canvas 
     x = constrain (x, 0, 1400);
@@ -183,4 +246,3 @@ function step(){
 }
 
 
-//
